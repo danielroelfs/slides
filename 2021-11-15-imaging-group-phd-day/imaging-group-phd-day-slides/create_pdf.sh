e3964535359cc25195140311c9c081070a5486b5
Daniel Roelfs <daniel@roelfs.net>
2021-11-14 12:37:37 +0100

    clean up phd day slides

--- 2021-11-15-imaging-group-phd-day/imaging-group-phd-day-slides/create_pdf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-
-# from: https://github.com/astefanutti/decktape
-
-docker run --rm -t -v `pwd`:/slides -v ~:/home/user astefanutti/decktape --size 1440x900 --pause 5000 index.html  imaging-group-phd-day-slides.pdf

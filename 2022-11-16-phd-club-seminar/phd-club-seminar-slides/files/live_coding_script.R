20032f77fec41c3ce48fb009792bb840ee0fd878
Daniel Roelfs <daniel@roelfs.net>
2022-11-16 16:00:08 +0100

    reorder slides and final changes before presentation

---
+++ 2022-11-16-phd-club-seminar/phd-club-seminar-slides/files/live_coding_script.R
@@ -0,0 +1,10 @@
+### PHD CLUB SEMINAR LIVE CODING ########################
+
+#-- Libraries -------------------------
+
+library(tidyverse)
+library(ggtext)
+library(scico)
+
+#-- Load data ------------------------
+

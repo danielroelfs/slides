925a36941f469be41dce85280148865df398fc7f
Daniel Roelfs <daniel@roelfs.net>
2023-10-27 16:55:04 +0200

    add analytics link

--- script.js
+++ script.js
@@ -23,4 +23,10 @@ window.onload = function() {
     link.rel = "icon";
     link.href = "https://danielroelfs.com/images/avatar.png";
     top.document.getElementsByTagName("head")[0].appendChild(link);
+
+    var umamiscript = document.createElement("script");
+    umamiscript.async = true
+    umamiscript.src = "https://analytics-danielroelfs.netlify.app/script.js";
+    umamiscript.setAttribute("data-website-id", "da48a88a-2e87-4024-8c99-639222aab54d");
+    document.head.appendChild(umamiscript);
 }
